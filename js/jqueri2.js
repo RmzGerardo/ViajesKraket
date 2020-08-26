875546e05924473b3b2e5739adcc15f810101fc5
Gerardo Ramirez <61709895+RmzGerardo@users.noreply.github.com>
2020-08-26 17:04:03 -0500

    Pagina web
    pagina web

---
+++ js/jqueri2.js
@@ -0,0 +1,39 @@
+function agregarBorde(){
+    document.getElementById("chef1").style.border = "10px solid #595963";
+    document.getElementById("descImg2").innerHTML= "Edu Ramirez / Jefe de Gaffers ";
+
+}
+
+function quitarBorde(){
+   document.getElementById("chef1").style.border = "none";
+   document.getElementById("descImg2").innerHTML= "";
+ 
+
+}
+
+
+function agregar(){
+    document.getElementById("chef2").style.border = "10px solid #595963";
+    document.getElementById("descImg3").innerHTML= "Gerardo Ramirez / Gaffer Junior";
+    
+}
+
+function quitar(){
+   document.getElementById("chef2").style.border = "none";
+   document.getElementById("descImg3").innerHTML= "";
+ 
+
+}
+
+
+function Bor(){
+    document.getElementById("chef3").style.border = "10px solid #595963";
+    document.getElementById("descImg4").innerHTML= "Amauri Tinoco / Jefe de Camionetas";
+
+}
+
+function Bo(){
+   document.getElementById("chef3").style.border = "none";
+   document.getElementById("descImg4").innerHTML= "";
+ 
+}
